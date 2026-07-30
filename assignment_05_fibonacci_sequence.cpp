bb4d23c2280f5e7bc34d689b433c6297de5a9c72
saaka-rukaya <rsaaka006@st.ug.edu.gh>
2026-07-30 17:12:14 +0000

    i wrote a c++ program with two parts,each implimented as a function

--- assignment_05_fibonacci_sequence.cpp
+++ assignment_05_fibonacci_sequence.cpp
@@ -51,3 +51,88 @@
 #include <iostream>
 using namespace std;
 
+void printFibonacci(int number_of_terms)
+{
+    if (number_of_terms <= 0)
+    {
+        cout << "Error: Number of terms must be a positive integer." << endl;
+        return;
+    }
+
+    int first_number = 0;
+    int second_number = 1;
+
+    cout << "Fibonacci sequence: ";
+
+    for (int term_number = 1; term_number <= number_of_terms; term_number++)
+    {
+        cout << first_number;
+
+        if (term_number < number_of_terms)
+        {
+            cout << " ";
+        }
+
+        int next_number = first_number + second_number;
+        first_number = second_number;
+        second_number = next_number;
+    }
+
+    cout << endl;
+}
+
+
+bool isFibonacci(int number_to_check)
+{
+    if (number_to_check < 0)
+    {
+        return false;
+    }
+
+    int first_number = 0;
+    int second_number = 1;
+
+    while (first_number < number_to_check)
+    {
+        int next_number = first_number + second_number;
+        first_number = second_number;
+        second_number = next_number;
+    }
+
+    if (first_number == number_to_check)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+int main()
+{
+    int number_of_terms;
+    int number_to_check;
+
+    // Part A
+    cout << "How many terms? ";
+    cin >> number_of_terms;
+
+    printFibonacci(number_of_terms);
+
+    // Part B
+    cout << "\nEnter a number to check: ";
+    cin >> number_to_check;
+
+    if (isFibonacci(number_to_check))
+    {
+        cout << number_to_check << " is a Fibonacci number." << endl;
+    }
+    else
+    {
+        cout << number_to_check << " is NOT a Fibonacci number." << endl;
+    }
+
+    return 0;
+}
