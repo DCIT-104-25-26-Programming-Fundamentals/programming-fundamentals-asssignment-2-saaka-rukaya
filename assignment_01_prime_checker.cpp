951f4522e9a1f27c131d0aea67f54b64c8b4224f
saaka-rukaya <rsaaka006@st.ug.edu.gh>
2026-07-30 16:44:31 +0000

    i wrote a c++ programe that checks whether a given number is prime

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,43 @@
 #include <iostream>
 using namespace std;
 
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int number)
+{
+    if (number < 2)
+    {
+        return false;
+    }
+
+    for (int divisor = 2; divisor < number; divisor++)
+    {
+        if (number % divisor == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (isPrime(number))
+    {
+        cout << number << " is a prime number." << endl;
+    }
+    else
+    {
+        cout << number << " is NOT a prime number." << endl;
+    }
+
+    return 0;
+}
